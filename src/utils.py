ee400758c89f086ae566945ee63bf17a347345a4
Fernando Schettini <fernandoschettini@Outlook.com>
2023-12-26 19:55:03 -0300

    Mensagem do commit

---
+++ src/utils.py
@@ -0,0 +1,12 @@
+"""
+File:           utils.py
+Last changed:   24/12/2023 13:24
+Purpose:        here are the static utils functions that will be used on all sets of tha application         
+Authors:        Fernando Antonio Marques Schettini           
+""" 
+
+class utils:
+
+    @staticmethod
+    def meu_metodo_estatico():
+        return "Este é um método estático!"
