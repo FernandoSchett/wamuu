ad279ad6449701dce117d0ef89e7d002536f6e53
Fernando Schettini <fernandoschettini@Outlook.com>
2024-01-05 12:13:47 -0300

    [CHORE]: now setup.py working with execute instructions how to upload project

--- setup.py
+++ setup.py
@@ -4,7 +4,8 @@ Last changed:   05/01/2024 11:43
 Purpose:        Code configures the distribution of a Python package using setuptools, including package details 
 Authors:        Fernando Antonio Marques Schettini      
 Usage: 
-	HowToExecute:   python3 setup.py sdist bdist_wheel        
+	HowToExecute:   python3 setup.py sdist bdist_wheel
+    HowToUpload: twine upload --repository-url https://upload.pypi.org/legacy/ -u __token__ -p your-token dist/*        
 """
 
 from setuptools import setup, find_packages
@@ -14,7 +15,7 @@ DESCRIPTION = 'Find best cable positioning'
 LONG_DESCRIPTION = 'Support tool, offers a set of Python classes designed for forecasting optimal wind turbine placements and efficient cable connections in wind energy systems.'
 
 authors = ["Fernando Schettini", "Pedro Miranda"]
-emails = ["<fernandoschettini@outlook.com>", "<mdeapedro@gmail.com>"]
+emails = "fernandoschettini@outlook.com"
 
 # Setting up
 setup(
@@ -30,8 +31,11 @@ setup(
     keywords=['python', 'wind', 'energy', 'cable positioning', 'search algorithm', 'optimization'],
     classifiers=[
         "Development Status :: 1 - Planning",
-        "Intended Audience :: Engineers",
-        "Programming Language :: Python :: 3",
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Developers',
+        'Topic :: Scientific/Engineering',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
         "Operating System :: Unix",
         "Operating System :: MacOS :: MacOS X",
         "Operating System :: Microsoft :: Windows",
