0dc947e4be76ec00dd75dedd69deebe5a0bf836a
Fernando <fernandoschettini@outlook.com>
2024-02-07 04:55:12 -0300

    [FIX]: c turbines forced to connect to subs

--- wamuu/grasp.py
+++ wamuu/grasp.py
@@ -4,21 +4,21 @@ from cost import cost
 
 def grasp(instance, starting_node, list_size, probability, flag=False):
     nodes = [i for i in range(1, instance.n + 1)]   
-    edges_group = grasp_edges(instance, nodes, starting_node, instance.dist, list_size, probability, flag)
+    edges_group = grasp_edges(nodes, starting_node, instance.dist, list_size, probability)
     nodes.remove(starting_node)
     nodes.insert(0,starting_node)
     power = get_turb_out_power(nodes, edges_group)
     edges = edges_group
     edges.append([starting_node, 0])  
-    
+
+    if flag == True:
+        nodes = sort_group_by_subst_dist(nodes, instance.dist)
+        for i in range(instance.C):
+            edges.append([nodes[i], 0])
+
     return [((edge[0], edge[1]), power[edge[0]]) for edge in edges]
 
-def grasp_edges(instance, nodes, starting_node, weight, list_size, probability, flag):
-    edges = []
-    if flag == True:
-        for i in range(instance.C):
-            edges.append([0, nodes[i]])
-         
+def grasp_edges(nodes, starting_node, weight, list_size, probability):
     def calculate_quality(mnode, fnode):
         return weight[mnode][fnode]
 
@@ -26,7 +26,7 @@ def grasp_edges(instance, nodes, starting_node, weight, list_size, probability, 
     fringe_nodes = {node for node in nodes}
     fringe_nodes.remove(starting_node)
 
-    
+    edges = []
     while len(fringe_nodes) > 0:
         candidate_list = []
         for mnode in mst_nodes:
@@ -46,19 +46,4 @@ def grasp_edges(instance, nodes, starting_node, weight, list_size, probability, 
             fringe_nodes.remove(next_fringe_node)
             mst_nodes.add(next_fringe_node)
             edges.append([next_fringe_node, next_mst_node])
-
     return edges
-
-def best_grasp(instance, nodes, weight, starting_node, len_rcl, probability, max_iterations=100):
-    best_solution = None
-    best_cost = float('inf')
-
-    for _ in range(max_iterations):
-        current_solution = grasp(nodes, weight, starting_node, len_rcl, probability)
-        current_cost = cost(instance ,current_solution, simple=True)
-
-        if current_cost < best_cost:
-            best_solution = current_solution
-            best_cost = current_cost
-
-    return best_solution
